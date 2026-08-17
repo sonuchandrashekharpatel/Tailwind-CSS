cc5a5b38c9813ed928c9316908b8afdbe78f95d5
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-08-17 09:34:06 +0530

    Scaffold and Started Tailwind CSS

---
+++ Courses/Learn Tailwind CSS/index.js
@@ -0,0 +1,95 @@
+/* Chapter - 1: Learn Tailwind CSS */
+
+/* Lesson 21: Want to become a Scrimbassador? */
+
+
+/* Lesson 20: Congrats */
+
+
+/* Lesson 19: Desktop layout */
+
+
+/* Lesson 18: Responsive Design */
+
+
+/* Lesson 17: Extra Styling */
+
+
+/* Lesson 16: Modifiers */
+
+
+/* Lesson 15: Other common utilities */
+
+
+/* Lesson 14: Layout */
+
+
+/* Lesson 13: Borders */
+
+
+/* Lesson 12: Flexbox */
+
+
+/* Lesson 11: Display and Position */
+
+
+/* Lesson 10: Typography */
+
+
+/* Lesson 9: Other Typography Classes */
+
+
+/* Lesson 8: Text Classes */
+
+
+/* Lesson 7: Font Classes */
+
+
+/* Lesson 6: Header and Footer */
+
+
+/* Lesson 5: Padding and Margin */
+
+
+/* Lesson 4: Colours */
+
+
+/* Lesson 3: Element Sizing */
+
+
+/* Lesson 2: What is Tailwind CSS? */
+
+
+/* Lesson 1: Welcome to Learn Tailwind CSS! */
+
+
+
+/* 
+import lessonGenerator from "../../../utils/index.js"
+
+const chapterName = "Learn Tailwind CSS"
+const chapterNum = 1
+const lesson = [
+    "Welcome to Learn Tailwind CSS!",
+    "What is Tailwind CSS?",
+    "Element Sizing",
+    "Colours",
+    "Padding and Margin",
+    "Header and Footer",
+    "Font Classes",
+    "Text Classes",
+    "Other Typography Classes",
+    "Typography",
+    "Display and Position",
+    "Flexbox",
+    "Borders",
+    "Layout",
+    "Other common utilities",
+    "Modifiers",
+    "Extra Styling",
+    "Responsive Design",
+    "Desktop layout",
+    "Congrats",
+    "Want to become a Scrimbassador?"
+]
+lessonGenerator(chapterName, lesson, chapterNum)  */
