e62e6aabb360b5cde97e0262b92c73bdf88820d8
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-08-19 18:54:10 +0530

    Starting the challenges of Tailwind CSS

---
+++ Courses/Tailwind CSS Challenges/index.js
@@ -0,0 +1,47 @@
+/* Chapter - 2: Tailwind CSS Challenges */
+
+/* Lesson 14: Certificate of Completion */
+
+
+/* Lesson 13: How to Utilize Your Certificate */
+
+
+/* Lesson 12: What's Next? */
+
+
+/* Lesson 11: Tailwind CSS - 404 Page Layout Challenge Possible Solution */
+
+
+/* Lesson 10: Tailwind CSS - 404 Page Layout Challenge */
+
+
+/* Lesson 9: Tailwind CSS - Preferences Form Challenge Possible Solution */
+
+
+/* Lesson 8: Tailwind CSS - Preferences Form Challenge */
+
+
+/* Lesson 7: Tailwind CSS - Pricing Table Challenge Possible Solution */
+
+
+/* Lesson 6: Tailwind CSS - Pricing Table Challenge */
+
+
+/* Lesson 5: Tailwind CSS - Testimonials Section Challenge Possible Solution */
+
+
+/* Lesson 4: Tailwind CSS - Testimonials Section Challenge */
+
+
+/* Lesson 3: Tailwind CSS - Pomodoro App Challenge Possible Solution */
+
+
+/* Lesson 2: Tailwind CSS - Pomodoro App Challenge */
+
+
+/* Lesson 1: Welcome to Tailwind CSS Challenges */
+/* 
+Tailwind CSS Challenges
+Teacher: Rachel Johnson
+
+*/
